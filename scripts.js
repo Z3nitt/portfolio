645e4c990ea260df664f7ad76b239986bc9b6fe3
Z3nitt <helios1612@gmail.com>
2025-01-29 21:28:43 +0100

    v1.0

---
+++ scripts.js
@@ -0,0 +1,22 @@
+let lastScrollTop = 0; // Variable para almacenar la última posición de scroll
+const header = document.querySelector('header'); // Seleccionamos el header
+
+// Detectar cuando se hace scroll
+window.addEventListener('scroll', function() {
+    let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+    
+    // Solo ejecutar el código si la página tiene suficiente contenido para hacer scroll
+    if (document.body.scrollHeight > window.innerHeight) {
+        if (currentScroll > lastScrollTop) {
+            // Si estamos desplazándonos hacia abajo, ocultar el header
+            header.style.top = '-60px'; // Cambiar este valor si el height del header es diferente
+        } else {
+            // Si estamos desplazándonos hacia arriba, mostrar el header
+            header.style.top = '0';
+        }
+        lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // No permitir scroll hacia arriba cuando se llega al tope
+    } else {
+        // Si no hay suficiente contenido (no hay scroll), aseguramos que el header siempre esté visible
+        header.style.top = '0';
+    }
+});
